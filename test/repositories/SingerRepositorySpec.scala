9a8257e828a3ac405c197e7b59a0afa629d1a0e2
Hugh Barrigan <hugh.barrigan@tapjoy.com>
2014-09-21 19:50:15 -0400

    Added singer specs

--- test/repositories/SingerRepositorySpec.scala
+++ test/repositories/SingerRepositorySpec.scala
@@ -15,7 +15,8 @@ class SingerRepositorySpec extends DBSpecBase {
   "SingerRepository#joinSession" when {
     "given a valid session id" when {
       class ExistingSession(implicit val dbSession: DBSession) extends SingerSpecHelpers {
-        val sessionId = sessionRepository.save(Session(name = "test session", userId = UserId(1)))
+        def password: Option[String] = None
+        val sessionId = sessionRepository.save(Session(name = "test session", userId = UserId(1), password = password))
         val session = sessionRepository.findById(sessionId).get
       }
 
@@ -43,16 +44,34 @@ class SingerRepositorySpec extends DBSpecBase {
               singerRepository.joinSession(JoinSessionRequest(sessionId, "Bob")) shouldBe Failure(NameAlreadyTaken("Bob"))
             }
           }
-
         }
       }
 
       "there is a password" when {
-        "the password is invalid" should {
+        class WithPassword(override implicit val dbSession: DBSession) extends ExistingSession()(dbSession) {
+          override def password = Some("password")
 
         }
 
+        "the password is invalid" should {
+          "return bad password error" in { s =>
+            new WithPassword()(s) {
+              singerRepository.joinSession(JoinSessionRequest(sessionId, "Bob", password = Some("blah"))) shouldBe Failure(InvalidPassword(sessionId))
+            }
+          }
+        }
+
         "the password is correct" should {
+          "create the singer successfully" in { s =>
+            new WithPassword()(s) { // The setup steps require a session, which we pass in like this to make the implicits work
+              singerRepository.joinSession(JoinSessionRequest(sessionId, "Bob", password = Some("password"))) match {
+                case Success(singer) =>
+                  singer.id shouldNot be(None)
+                  singer.name shouldBe "Bob"
+                case m => fail("no singer " + m)
+              }
+            }
+          }
 
         }
       }
